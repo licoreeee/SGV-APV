bafc0ef8b1834b5a149ea6dcc810e97e77c8d4ed
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2025-03-19 22:25:59 -0700

    commit

--- AccesoDatos_SGVAPV/src/main/java/org/itson/accesodatos_sgvapv/daos/ProductosDAO.java
+++ AccesoDatos_SGVAPV/src/main/java/org/itson/accesodatos_sgvapv/daos/ProductosDAO.java
@@ -1,6 +1,6 @@
-package org.itson.accesodatos_sgvapv;
+package org.itson.accesodatos_sgvapv.daos;
 
-public class AccesoDatos_SGVAPV {
+public class ProductosDAO {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
