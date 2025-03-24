bf3b6573d3eae18397faf42351b99eb4d8fa486d
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2025-03-23 22:13:40 -0700

    proyecto de dtos creado

---
+++ DTOs_SGVAPV/src/main/java/org/itson/dtos_sgvapv/DTOs_SGVAPV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.dtos_sgvapv;
+
+/**
+ *
+ * @author ricar
+ */
+public class DTOs_SGVAPV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
